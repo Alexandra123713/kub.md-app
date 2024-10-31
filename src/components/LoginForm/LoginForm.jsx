7d71240e8d32b373bd0a9f5b62114bd70972d4c1
Alexandra Cerevatii <alexandracerevatii@gmail.com>
2024-10-31 17:43:56 +0200

    Added FormContainer and SignInButton component

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,37 @@
+import styled from 'styled-components';
 import { LoginNavBar } from './LoginNavBar';
+import { SignInButton } from './SignInButton';
 
 export const LoginForm = () => {
   return (
     <>
       <LoginNavBar />
+      <FormContainer>
+        <SignInInput
+          type='text'
+          placeholder='Enter login'
+        />
+        <SignInInput
+          type='text'
+          placeholder='Enter passwort'
+        />
+        <SignInButton />
+      </FormContainer>
     </>
   );
 };
+
+const FormContainer = styled.form`
+  margin: 16rem auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 20rem;
+  gap: 4rem;
+`;
+
+const SignInInput = styled.input`
+  width: 16rem;
+  height: 3rem;
+  padding: 0 1rem;
+`;
---
+++ src/components/LoginForm/SignInButton.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const SignInButton = () => {
+  return <Button>Sign in</Button>;
+};
+
+const Button = styled.button`
+  background-color: #96c747;
+  border-radius: 30px;
+  width: 16rem;
+  height: 5rem;
+  display: block;
+  font-size: 2rem;
+  cursor: pointer;
+`;
